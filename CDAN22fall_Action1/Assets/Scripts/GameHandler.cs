05a7d6bf7aab52176ded16d0240d5259d40e4aa4
mangoostarr <agalimagno@massart.edu>
2022-11-14 22:26:21 -0500

    Added Pause Menu, Credits, Win Scene, Lose Scene
    made Pause Menu a prefab too

--- CDAN22fall_Action1/Assets/Scripts/GameHandler.cs
+++ CDAN22fall_Action1/Assets/Scripts/GameHandler.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class GameHandler : MonoBehaviour {
 
@@ -20,6 +21,49 @@ public class GameHandler : MonoBehaviour {
       public static bool stairCaseUnlocked = false;
       //this is a flag check. Add to other scripts: GameHandler.stairCaseUnlocked = true;
 
+      public static bool GameisPaused = false;
+        public GameObject pauseMenuUI;
+        public AudioMixer mixer;
+        public static float volumeLevel = 1.0f;
+        private Slider sliderVolumeCtrl;
+
+        void Awake (){
+                SetLevel (volumeLevel);
+                GameObject sliderTemp = GameObject.FindWithTag("PauseMenuSlider");
+                if (sliderTemp != null){
+                        sliderVolumeCtrl = sliderTemp.GetComponent<Slider>();
+                        sliderVolumeCtrl.value = volumeLevel;
+                }
+        }
+
+        void Update (){
+                if (Input.GetKeyDown(KeyCode.Escape)){
+                        if (GameisPaused){
+                                Resume();
+                        }
+                        else{
+                                Pause();
+                        }
+                }
+        }
+
+        void Pause(){
+                pauseMenuUI.SetActive(true);
+                Time.timeScale = 0f;
+                GameisPaused = true;
+        }
+
+        public void Resume(){
+                pauseMenuUI.SetActive(false);
+                Time.timeScale = 1f;
+                GameisPaused = false;
+        }
+
+        public void SetLevel (float sliderValue){
+                mixer.SetFloat("MusicVolume", Mathf.Log10 (sliderValue) * 20);
+                volumeLevel = sliderValue;
+        }
+
       private string sceneName;
 
       void Start(){
@@ -28,6 +72,8 @@ public class GameHandler : MonoBehaviour {
             //if (sceneName=="MainMenu"){ //uncomment these two lines when the MainMenu exists
                   playerHealth = StartPlayerHealth;
             //}
+            pauseMenuUI.SetActive(false);
+            GameisPaused = false;
             updateStatsDisplay();
       }
 
