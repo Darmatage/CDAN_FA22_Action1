cee5259e46aba96e3b6471e0a0f086536ed41b03
Darmatage <JasonWiserArt@gmail.com>
2022-12-22 02:52:40 -0500

    Mountain level progress, with banjo wizard

--- CDAN22fall_Action1/Assets/Scripts/NPCDialogue.cs
+++ CDAN22fall_Action1/Assets/Scripts/NPCDialogue.cs
@@ -4,32 +4,40 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class NPCDialogue : MonoBehaviour {
-       private Animator anim;
-       private NPCDialogueManager dialogueMNGR;
-       public string[] dialogue; //enter dialogue lines into the inspector for each NPC
-       public bool playerInRange = false; //could be used to display an image: hit [e] to talk
-       public int dialogueLength;
+	private Animator anim;
+	private NPCDialogueManager dialogueMNGR;
+	public string[] dialogue; //enter dialogue lines into the inspector for each NPC
+	public bool playerInRange = false; //could be used to display an image: hit [e] to talk
+	public int dialogueLength;
+	public bool isBanjo = false;
 
-       void Start(){
+	void Start(){
               anim = gameObject.GetComponentInChildren<Animator>();
               dialogueLength = dialogue.Length;
 			  dialogueMNGR = GameObject.FindWithTag("GameHandler").GetComponent<NPCDialogueManager>();
               // if (GameObject.FindWithTag("DialogueManager")!= null){
                      // dialogueMNGR = GameObject.FindWithTag("DialogueManager").GetComponent<NPCDialogueManager>();
               // }
-       }
+			  
+		if (isBanjo){
+			anim.SetBool("banjo", true);
+		} else {
+			anim.SetBool("banjo", false);
+		}
+	}
 
-       private void OnTriggerEnter2D(Collider2D other){
+
+	private void OnTriggerEnter2D(Collider2D other){
 		   if (other.gameObject.transform.parent != null){
               if (other.gameObject.transform.parent.tag == "Player") {
                      playerInRange = true;
                      dialogueMNGR.LoadDialogueArray(dialogue, dialogueLength);
                      dialogueMNGR.OpenDialogue();
-                     anim.SetBool("chat", true);
+                     if (!isBanjo){ anim.SetBool("chat", true);}
                      //Debug.Log("Player in range");
-              }
-		   }
-       }
+			}
+		}
+	}
 
        private void OnTriggerExit2D(Collider2D other){
 		   if (other.gameObject.transform.parent != null){
